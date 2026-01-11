e114d2d812ed30d83d09c853816395387ec3807f
Richi <ruechan.sixt@proton.me>
2026-01-11 01:10:03 +0100

    update

--- .config/nvim/lua/plugins/code-runner.lua
+++ .config/nvim/lua/plugins/code-runner.lua
@@ -2,7 +2,6 @@ return {
 	"hkupty/iron.nvim",
 	config = function()
 		local iron = require("iron.core")
-		local view = require("iron.view")
 
 		iron.setup({
 			config = {
@@ -22,7 +21,7 @@ return {
 				dap_integration = true,
 				-- How the repl window will be displayed
 				-- See below for more information
-				repl_open_cmd = require("iron.view").bottom(20),
+				repl_open_cmd = require("iron.view").right(40),
 			},
 			-- Iron doesn't set keymaps by default anymore.
 			-- You can set them here or manually add keymaps to the functions in iron.core
