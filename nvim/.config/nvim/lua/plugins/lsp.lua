8917d575620776a35829039aa8493cd7e2cc9f68
richi-sixt <ruechan.sixt@proton.me>
2026-02-23 16:20:25 +0100

    uninstalled ruff

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -35,38 +35,6 @@ return {
 						},
 					},
 				},
-				-- Ruff LSP (optional, wenn du Ruff als LSP willst)
-				ruff_lsp = {
-					init_options = {
-						settings = {
-							lineLength = 79,
-						},
-					},
-				},
-			},
-		},
-	},
-
-	-- Formatierung mit Ruff
-	{
-		"stevearc/conform.nvim",
-		opts = {
-			formatters_by_ft = {
-				python = { "ruff_format", "ruff_fix" },
-			},
-			format_on_save = {
-				timeout_ms = 500,
-				lsp_fallback = true,
-			},
-		},
-	},
-
-	-- Linting mit Ruff
-	{
-		"mfussenegger/nvim-lint",
-		opts = {
-			linters_by_ft = {
-				python = { "ruff" },
 			},
 		},
 	},
